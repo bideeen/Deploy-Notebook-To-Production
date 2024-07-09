975766bf885f157e12a5eec44f4b128687ed6e31
Miki Tebeka <miki@353solutions.com>
2024-07-09 13:46:08 +0000

    abspath

--- Ch03/03_02/rides_test.py
+++ Ch03/03_02/rides_test.py
@@ -1,8 +1,12 @@
+from pathlib import Path
 from subprocess import run
 import sys
 
+root_dir = Path(__file__).parent.absolute()
+
 
 def test_nb(tmp_path):
+    nb_file = root_dir / 'rides.ipynb'
     out_file = tmp_path / 'rides.html'
 
     cmd = [
@@ -11,7 +15,7 @@ def test_nb(tmp_path):
         '--execute',
         '--to', 'html',
         '--output', str(out_file),
-        'rides.ipynb',
+        str(nb_file),
     ]
     out = run(cmd)
     assert out.returncode == 0
